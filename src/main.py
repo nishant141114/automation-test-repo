9d0b55212835a5c21c2d07f55fb5ad2970a82442
nishant141114 <nishant.laxkar@nike.com>
2024-08-29 14:28:30 +0530

    Your commit message

--- src/main.py
+++ src/main.py
@@ -13,3 +13,11 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': 'Hello from Lambda!'
     }
+
+
+
+# git remote set-url origin https://github.com/NEW_USERNAME/NEW_REPO.git
+
+# git add .
+# git commit -m "Your commit message"
+# git push origin main
