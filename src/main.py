87e392491ab385422daa811ece50bc079c27e6b6
nishant141114 <nishant.laxkar@nike.com>
2024-08-29 14:38:37 +0530

    Your commit message

--- src/main.py
+++ src/main.py
@@ -16,8 +16,16 @@ def lambda_handler(event, context):
 
 
 
-# git remote set-url origin https://github.com/NEW_USERNAME/NEW_REPO.git
-
+# git remote set-url origin https://github.com/nishant141114/auto-using-workflow.git
 # git add .
 # git commit -m "Your commit message"
 # git push origin main
+
+# git credential-cache exit
+
+
+# Remove the existing credentials from the macOS Keychain:
+
+# Open the Keychain Access application on your Mac.
+# Search for github.com.
+# Delete any entries related to github.com.
